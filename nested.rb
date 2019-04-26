9fba77f353f70657724e2b7c6e2739d123ebd98d
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-26 18:33:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchies_language][:languages]
+		 programmer_hash[:dennis_ritchies_language][:languages].first
 end
 
 def adding_matz
